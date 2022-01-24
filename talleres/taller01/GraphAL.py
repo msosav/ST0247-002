d9ad7ed8a74ef0cabbf77f010b93bc72b6af76aa
Miguel S <85181687+msosav@users.noreply.github.com>
2022-01-24 16:47:58 -0500

    Add files via upload

--- talleres/taller01/GraphAL.py
+++ talleres/taller01/GraphAL.py
@@ -1,16 +1,27 @@
+from collections import deque
+
 class GraphAL:
     def __init__(self, size):
-
+        self.arregloDeListas = [0]*size
+        for i in range(0,size):
+            self.arregloDeListas[i] = deque()
 
     def addArc(self, vertex, edge, weight):
-
+        fila = self.arregloDeListas[vertex]
+        parejaDestinoPeso = (edge, weight)
+        fila.append(parejaDestinoPeso)
 
     def getSuccessors(self, vertice):
-
+        lista = []
+        arreglo = self.arregloDeListas[vertice]
+        for i in arreglo:
+            if i[1] != 0:
+                lista.append(i[0])
+        return lista
 
     def getWeight(self, source, destination):
-
-
-    def __str__(self):
-
-
+        arreglo = self.arregloDeListas[source]
+        for i in arreglo:
+            if i[0] == destination:
+                peso = i[1]
+        return peso
--- talleres/taller01/GraphAM.py
+++ talleres/taller01/GraphAM.py
@@ -1,17 +1,29 @@
-class GraphAm:
+import numpy as np
+
+
+class GraphAM:
 
     def __init__(self, size):
-
-
-    def getEdges(self):
-
+        dimensiones = (size, size)
+        self.matriz = np.zeros((size, size))
 
     def getWeight(self, source, destination):
-
+        return self.matriz[source][destination]
 
     def addArc(self, source, destination, weight):
+        self.matriz[source][destination] = weight
 
     def getSuccessors(self, vertex):
+        lista = []
+        for i in range(len(self.matriz[0])):
+            if self.matriz[vertex][i] != 0:
+                lista.append(i)
+        return lista
 
-    def __str__(self):
-
+class Lol:
+    g = GraphAM(4)
+    g.addArc(1,2,4)
+    g.addArc(1,3,10)
+    print(g.getWeight(1,2))
+    print(g.getWeight(1,3))
+    print(g.getSuccessors(1))

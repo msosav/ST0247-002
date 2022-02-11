4ef1d8ac9de240a387c3515bbd60a0fbe0123085
Miguel S <85181687+msosav@users.noreply.github.com>
2022-02-11 16:30:19 -0500

    Add files via upload

--- laboratorios/lab01/codigo/Punto1.py
+++ laboratorios/lab01/codigo/Punto1.py
@@ -1,11 +1,32 @@
+from collections import deque
+
 class GraphAL:
     def __init__(self, size):
+        self.nombreVertice = [""]*size
+        self.mapaCiudad = [0]*size
+        for i in range(0,size):
+            self.mapaCiudad[i] = deque()
 
+    def addArc(self, id1, id2, distancia):
+        fila = self.mapaCiudad[id1]
+        parejaDestinoPeso = (id2, distancia)
+        fila.append(parejaDestinoPeso)
 
-    def addArc(self, vertex, edge, weight):
+    def getWeight(self, id1, id2):
+        arreglo = self.mapaCiudad[id1]
+        for i in arreglo:
+            if i[0] == id2:
+                peso = i[1]
+        return peso
 
+    def addVertex(self, id, name):
+        self.nombreVertice[id] = name
 
-    def getSuccessors(self, vertice):
+def main():
+    g = GraphAL(5)
+    g.addVertex(1, "Movies")
+    g.addVertex(2, "Snell")
+    g.addArc(1, 2, 10)
+    print(g.getWeight(1,2))
 
-
-    def getWeight(self, source, destination):
+main()

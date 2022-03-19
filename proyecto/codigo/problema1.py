9ea641305cba38c2c79411ce33cdce8d99b87e29
Miguel S <85181687+msosav@users.noreply.github.com>
2022-03-19 16:34:11 -0500

    Add files via upload

--- proyecto/codigo/problema1.py
+++ proyecto/codigo/problema1.py
@@ -1,5 +1,5 @@
-from cmath import inf
 import pandas as pd
+import numpy as np
 
 from collections import deque
 
@@ -31,61 +31,13 @@ class GraphAL:
                 peso = i[1]
         return peso
 
-def rellenar(lista, df, nombreCol):
-    for i in len(df[nombreCol]):
-        lista[i]=(df["name"], df["origin"], df["destination"], df["length"], df["oneway"], df["harassmentRisk"],)
-    return lista
-
-def idDestino(df, destino, nombres):
-    contador = 0
-    for i in df["origin"]:
-        if i == destino:
-            return contador
-        contador += 1
-
-def djikstra(grafo, inicio:int) -> list:
-    distancias = [inf]*grafo.size
-    distancias[inicio] = 0
-    predecesores = [-1]*grafo.size
-    visitados = [False]*grafo.size
-    visitados[inicio] = True
-    vertice = inicio
-    for _ in range(grafo.size):
-        vertice = elMasCercaNoVisitado(grafo, vertice, distancias, predecesores, visitados)
-        visitados[vertice] = True
-        actualizarLaTabla(grafo, vertice, distancias, predecesores)
-    return (distancias, predecesores)
-
-def elMasCercaNoVisitado(grafo, vertice, distancia, predecesor, visitados):
-    losVecinosDeV = grafo.getSuccessors(vertice)
-    elMasCerca = 0
-    pesoElMasCerca = inf
-    peso = 0
-    for vecino in losVecinosDeV:
-        peso = grafo.getWeight(vertice, vecino)
-        if peso <= pesoElMasCerca and not visitados[vecino]:
-            elMasCerca = vecino
-            pesoElMasCerca = peso
-            predecesor[elMasCerca] = vertice
-            distancia[elMasCerca] = peso + distancia[vertice]
-    return elMasCerca
-
-def actualizarLaTabla(grafo, vertice, distancia, predecesor) -> None:
-    losVecinosDeV = grafo.getSuccessors(vertice)
-    for vecino in losVecinosDeV:
-        elPesoDeVAlVecino = grafo.getWeight(vertice, vecino)
-        if (distancia[vertice] + elPesoDeVAlVecino) < distancia[vecino]:
-            distancia[vecino] = distancia[vertice] + elPesoDeVAlVecino
-            predecesor[vecino] = vertice
-
 def main():
     df = pd.read_csv("calles_de_medellin_con_acoso.csv", sep=";")
-    lista = rellenar([0]*len(df["name"]), df, "name")
-    g = GraphAL(len(df["name"]))
-    for i in lista:
-        print(i)
-
-"""df = pd.read_csv("calles_de_medellin_con_acoso.csv", sep=";")
-print(df)"""
-
+    lista = df.to_numpy().tolist()
+    g = GraphAL(len(df))
+    for id in range(len(df)):
+        id2 = df.index[df["origin"] == lista[id][2]].tolist()
+        g.addArc(id, lista[id][2], lista[id][3])
+    print(g.getSuccessors(1))
+    
 main()
